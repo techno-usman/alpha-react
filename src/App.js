b91f293db7c08c93b492b7e7cefd52e4b3479ed9
Muhammad Usman Ch <developermolvi@gmail.com>
2022-08-31 23:01:09 +0500

    login page v1

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import Home from './pages/landing/Home'
 import News from './pages/news/News'
 import Blog from './pages/blog/Blog'
 import BlogDetails from './pages/blog/BlogDetails'
+import Login from './pages/login/Login'
 import NotFound from './pages/notFound/NotFound'
 
 function App() {
   return (
     <Routes>
       <Route path='/' element={<Home />} />
+      <Route path='/login' element={<Login />} />
       <Route path='/home/news' element={<News />} />
       <Route path='/home/blog' element={<Blog />} />
       <Route path='/home/blog/details' element={<BlogDetails />} />
